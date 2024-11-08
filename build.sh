0dbdc5f58a91a30b56a07226083cf3272e99d33c
zhanghaipeng <peregrine.calder@gmail.com>
2024-11-08 12:59:28 -0600

    Changes

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -t peregrinecalder/big-market-front-app:2.0 .
+#docker build --platform linux/amd64 -t peregrinecalder/big-market-front-app:2.0 .
---
+++ src/types/RaffleAwardVO.ts
@@ -0,0 +1,13 @@
+/**
+ * 策略奖品值对象
+ */
+export interface RaffleAwardVO {
+    awardId: number;
+    awardTitle: string;
+    awardSubtitle: string;
+    sort: number;
+    awardRuleLockCount: number;
+    isAwardUnlock: boolean;
+    waitUnLockCount: number;
+}
+
